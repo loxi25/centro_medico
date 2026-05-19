ba5ede5fddc547f783803cbc79a6971a382dc728
loxi25 <oscarsanchez1@colegioguanenta.edu.co>
2026-05-19 09:15:35 -0500

    se subio todo

---
+++ controlador/login_controlador.php
@@ -0,0 +1,32 @@
+<?php
+    require_once `.. /modelo/usuario_modelo.php`;
+
+    // mostrar formulario
+    function mostrar_login()
+    {
+        require_once `.. /vista/login_vista.php`;
+
+    }
+
+    // procesar credenciales
+    function autenticar()
+    {
+        if($_SERVER[`REQUEST_METHOD`] == `POST`)
+        {
+            $login = $_POST[`login`];
+            $pass  = $_POST[`password`];
+
+            $usuario = validar_usuario($login, $pass);
+
+            if($usuario)
+            {
+                header("location : index.php");
+            }
+            else 
+            {
+                echo `credenciales incorrectas`;
+            }
+
+        }
+
+    }
